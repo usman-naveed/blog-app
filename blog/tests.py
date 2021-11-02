94d2d2d9e48d36765e51d3eb252e8f78bf5e8f44
Mohammad Naveed <mohammadusman94@hotmail.ca>
2021-11-02 17:15:47 -0400

    added tests for new views (CRUD)

--- blog/tests.py
+++ blog/tests.py
@@ -34,3 +34,27 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+        'title': 'New title',
+        'body': 'New text',
+        'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().body, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+        'titie': 'Updated title',
+        'body': 'Updated body'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
